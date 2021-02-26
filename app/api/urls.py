fe9fb12205e4b49136ae5ac6f25994b9b82b3efb
lee Forster <lforster@hotmail.co.uk>
2021-02-26 20:57:17 +0000

    first commit

--- app/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import GameView
-
-router = routers.DefaultRouter()
-
-urlpatterns = [
-    path('games/', GameView.as_view()),
-]
